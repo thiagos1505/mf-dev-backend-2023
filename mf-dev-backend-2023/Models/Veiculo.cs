383a81097251644ad855720f2ef908c146eda35c
Thiago Singulano <thiagosingulano.dev@gmail.com>
2023-10-15 19:27:05 -0300

    feat: configura o DB e cria a tabela veiculos

---
+++ mf-dev-backend-2023/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2023.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "A placa é obrigatória")]
+        public string? Placa { get; set; }
+        
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        public string? Modelo { get; set; }
+        
+        [Required(ErrorMessage = "O ano de fabricação é obrigatório")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "O ano do modelo é obrigatório")]
+        public int AnoModelo { get; set; }
+    }
+}
