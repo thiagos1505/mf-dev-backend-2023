96bf742589a4f28c8353565e7f7c27a34dc29362
Thiago Singulano <thiagosingulano.dev@gmail.com>
2023-10-15 20:28:04 -0300

    feat: adicionar o create e edita itens basicos

--- mf-dev-backend-2023/Models/Veiculo.cs
+++ mf-dev-backend-2023/Models/Veiculo.cs
@@ -16,9 +16,11 @@ namespace mf_dev_backend_2023.Models
         public string? Modelo { get; set; }
         
         [Required(ErrorMessage = "O ano de fabricação é obrigatório")]
+        [Display(Name = "Ano de Fabricação")]
         public int AnoFabricacao { get; set; }
 
         [Required(ErrorMessage = "O ano do modelo é obrigatório")]
+        [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
     }
 }
