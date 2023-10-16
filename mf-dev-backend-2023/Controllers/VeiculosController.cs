12b4213636a79f45775d0c0b3b38c4fafef19099
Thiago Singulano <thiagosingulano.dev@gmail.com>
2023-10-16 18:50:19 -0300

    feat: finaliza o CRUD de veículos

--- mf-dev-backend-2023/Controllers/VeiculosController.cs
+++ mf-dev-backend-2023/Controllers/VeiculosController.cs
@@ -67,7 +67,52 @@ namespace mf_dev_backend_2023.Controllers
 
             return View(veiculo);
         }
-        
-              
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculos.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+                      
+            
+            return View(dados);
+        }
+       
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculos.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+            return View(dados);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (id == null)
+                return NotFound();
+            
+            var dados = await _context.Veiculos.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+            
+            _context.Veiculos.Remove(dados);
+           
+            await _context.SaveChangesAsync();
+           
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
